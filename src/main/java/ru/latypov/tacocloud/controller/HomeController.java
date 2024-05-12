44d5ac3672d30946d280a10f7e3f75bd76f92e0d
Mikhail <mikhaillat@yandex.ru>
2024-05-12 09:29:44 +0300

    Добавлен тест для домашней станицы

--- src/main/java/ru/latypov/tacocloud/controller/HomeController.java
+++ src/main/java/ru/latypov/tacocloud/controller/HomeController.java
@@ -1,4 +1,13 @@
 package ru.latypov.tacocloud.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
 }
